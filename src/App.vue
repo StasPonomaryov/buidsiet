c3d88816d9979b7cc563a16e9c5b6f3db11fd54b
StasPonomaryov <ponomaryov.stas@gmail.com>
2024-01-30 15:16:49 +0200

    zustand using

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { ref } from 'vue';
+import { getState, setState } from './store/store';
 import IncomesArticles from './components/IncomesArticles.vue';
 import ExpenditureArticles from "./components/ExpenditureArticles.vue";
-import { bArticles, expenditureDefault, incomesDefault } from "./__mocks__/db";
 
+const buidseitItems = ref(getState().buidseitItems);
+const expenditureItems = ref(getState().expenditureItems);
+const revenueItems = ref(getState().revenueItems);
+const balance = ref(getState().computed.balance());
+console.log('>>>BALANCE', getState().computed.balance());
 </script>
 
 <template>
@@ -11,18 +17,20 @@ import { bArticles, expenditureDefault, incomesDefault } from "./__mocks__/db";
       <div class="row">
         <div v-for="n in 3" :key="n" class="col">
           <div class="ma-2 pa-2">
-            <strong>{{ bArticles[n - 1].name }}</strong>
+            <strong>{{ buidseitItems[n - 1].name }}</strong>
           </div>
         </div>
       </div>
       <div class="row">
         <div class="col">
-          <ExpenditureArticles :articles="expenditureDefault" />
+          <ExpenditureArticles :articles="expenditureItems" />
         </div>
         <div class="col">
-          <IncomesArticles :articles="incomesDefault" />
+          <IncomesArticles :articles="revenueItems" />
         </div>
-        <div class="col"></div>
+        <div class="col">
+          {{ balance }}
+        </div>
       </div>
     </div>
   </div>
